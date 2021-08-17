688118f6accde57304e1725d5834542077478863
Fardin Mohammed <fardeenmohdpl@gmail.com>
2021-08-17 16:23:20 +0200

    Add first and the last commit

---
+++ src/data.js
@@ -0,0 +1,108 @@
+import { v4 as uuidv4 } from "uuid";
+
+function chillHop() {
+  return [
+    {
+      name: "Mario Kart",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/03/74d62bc9370a68e440c1b98eaf650344f0a7faea-1024x1024.jpg",
+      artist: "SwuM",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=15228",
+      color: ["#524129", "#915E25"],
+      id: uuidv4(),
+      active: true,
+    },
+    {
+      name: "Lonely Waves",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/02/80be32197ca7971424bef5e4d0c6810e1d4334c6-1024x1024.jpg",
+      artist: "C Y G N",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=14981",
+      color: ["#E386F3", "#0A102C"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "What If I Told You",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/07/4163ebb931e06d4ee8eb184295c0246d4a5055f7-1024x1024.jpg",
+      artist: "Juan Rios",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=21649",
+      color: ["#FD8A6D", "#634273"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Airplane Mode",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/06/23b2ff959731b56ea8545828b462311e8b1a2bcc-1024x1024.jpg",
+      artist: "Axian, falcxne, Makzo",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=20121",
+      color: ["#BABB54", "#0755BE"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Minus Five in Moscow",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/03/75adfe0661d06a9ea66d9c2e99b31e92ae450ebe-1024x1024.jpg",
+      artist: "Psalm Trees, ALEXANDER",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=16074",
+      color: ["#3EBBA3", "#F1E15E"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Memories Pt.1",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/04/cb0cc6270d7f2e1bb13e44e8832bd5c9b2a61080-1024x1024.jpg",
+      artist: "Evil Needle",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=21659",
+      color: ["#D9C6BD", "#EA4D4C"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Cascade",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/02/70a56749b8b89815fa75446030c6ba57d2c34de7-1024x1024.jpg",
+      artist: "Knowmadic",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=13092",
+      color: ["#376A53", "#DA9746"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Shine",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/07/034f52eeaeceb144cca67b0930d705d32fc39e37-1024x1024.jpg",
+      artist: "Ruck P, The Kount",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=20565",
+      color: ["#644E78", "#DD7F85"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "LuvnYou",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/07/875b9624506ae8d05750b9e4ab4579abb1fe3e16-1024x1024.jpg",
+      artist: "Chillhop Music",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=21784",
+      color: ["#92E7D8", "#CA6415"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Upstate",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/2899f7cc22ab12e17d0119819aac3ca9dbab46e6-1024x1024.jpg",
+      artist: "Philanthrope",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10245",
+      color: ["#92E7D8", "#CA6415"],
+      id: uuidv4(),
+      active: false,
+    },
+  ];
+}
+
+export default chillHop;
